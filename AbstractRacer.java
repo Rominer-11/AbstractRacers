39d6a0b8d26dfec4ed3ae34311d70d86a7791cc3
August Cho <acho26@students.polytechnic.org>
2024-03-09 21:39:13 -0800

    Implimented betting

--- AbstractRacer.java
+++ AbstractRacer.java
@@ -6,20 +6,24 @@
 
 public abstract class AbstractRacer
 {
+	static int ids = 0;
 	private String name;
 	private int position;
+	private int id;
 
 	public AbstractRacer(String name)
 	{
 		this.name = name;
 		this.position = 0;
+		this.id = ids;
+		ids++;
 	}
 
 	public abstract void move();
 
 	public String toString()
 	{
-		return "Racer[" + name + ", Position: " + position + "]";
+		return "Racer[ID " + id + ", " + name + ", Position: " + position + "]";
 	}
 
 	public int getPosition()
@@ -29,5 +33,9 @@ public abstract class AbstractRacer
 	public void setPosition(int position)
 	{
 		this.position = position;
+	}
+	public int getId()
+	{
+		return id;
 	}
 }
--- RaceRunner.java
+++ RaceRunner.java
@@ -9,28 +9,61 @@ import java.util.ArrayList;
 
 public class RaceRunner
 {
+	public static Scanner in;
+
 	public static void main(String[] args)
 	{
-		Scanner in = new Scanner(System.in);
+		in = new Scanner(System.in);
+		
+		System.out.println("How many users are betting?");
+		int[] userBalance = new int[in.nextInt()];
+		int[] userBets = new int[userBalance.length];
+		int[] userChoices = new int[userBalance.length];
+		for (int i = 0; i < userBalance.length; i++)
+		{
+			System.out.println("User " + i + " has 100 tortoise coins.");
+			userBalance[i] = 100;
+		}
+		
 		while (true)
 		{
 			System.out.println("Enter race length.");
 			int length = in.nextInt();
-			race(length);
+			race(length, userBalance, userBets, userChoices);
 		}
 	}
 	
-	public static void race(int length)
+	public static void race(int length, int[] userBalance, int[] userBets, int[] userChoices)
 	{
 		Race race = new Race(length);
 
 		boolean raceFinished = false;
 		ArrayList<AbstractRacer> winners = new ArrayList<AbstractRacer>();
-		
+		ArrayList<Integer> winningUsers = new ArrayList<Integer>();
+
 		race.addRacer(new Tortoise("Tortoiseerton"));
 		race.addRacer(new Hare("Hareerton"));
 		race.addRacer(new BananaKart("BananaKarterton"));
 
+		System.out.println("Here are the racers: ");
+		for (int i = 0; i < race.getRacers().size(); i++)
+		{
+			System.out.println(race.getRacers().get(i).toString());
+		}
+		System.out.print("\n");
+
+		for (int i = 0; i < userBalance.length; i++)
+		{
+			System.out.println("User " + i + ", who are you betting on?");
+			System.out.println("You have " + userBalance[i] + " tortoise coins.");
+			int bet = in.nextInt();
+			userChoices[i] = bet;
+			System.out.println("How much are you betting?");
+			int amount = in.nextInt();
+			userBets[i] = amount;
+			System.out.print("\n");
+		}
+		
 		while (!raceFinished)
 		{
 			winners = race.update();
@@ -46,7 +79,14 @@ public class RaceRunner
 			System.out.println("The winner is...");
 			for (AbstractRacer racer : winners)
 			{
-				System.out.println(racer.toString());
+				System.out.println(racer.toString() + "\n");
+				for (int i = 0; i < userChoices.length; i++)
+				{
+					if (userChoices[i] == racer.getId())
+					{
+						winningUsers.add(i);
+					}
+				}
 			}
 		}
 		else
@@ -54,8 +94,39 @@ public class RaceRunner
 			System.out.println("It's a tie! The winners are...");
 			for (AbstractRacer racer : winners)
 			{
-				System.out.println(racer.toString());
+				System.out.println(racer.toString() + "\n");
+				for (int i = 0; i < userChoices.length; i++)
+				{
+					if (userChoices[i] == racer.getId())
+					{
+						winningUsers.add(i);
+					}
+				}
 			}
+		}
+		int winnerSum = 0;
+		for (int i = 0; i < userBets.length; i++)
+		{
+			boolean winner = false;
+			for (int n : winningUsers)
+			{
+				if (i == n)
+				{
+					winner = true;
+				}
+
+			}
+			if (!winner)
+			{
+				userBalance[i] -= userBets[i];
+				winnerSum += userBets[i];
+				System.out.println("User " + i + " lost " + userBets[i] + " tortoise coins.\n");
+			}
+		}
+		for (int i = 0; i < winningUsers.size(); i++)
+		{
+			userBalance[winningUsers.get(i)] += winnerSum / winningUsers.size();
+			System.out.println("User " + winningUsers.get(i) + " gained " + (winnerSum / winningUsers.size()) + " tortoise coins.\n");
 		}
 	}
 }
